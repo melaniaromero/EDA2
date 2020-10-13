6cbefc6bf5edcec6a8b977d063ecb80cdf295016
melaniaromero <57497889+melaniaromero@users.noreply.github.com>
2020-10-12 22:48:10 -0500

    Add files via upload

---
+++ 6a.comparacion.py
@@ -0,0 +1,11 @@
+print ("\nINTRODUZCA DOS VALORES DISTINTOS")
+A=int(input("A="))
+B=int(input("B="))
+while(A==B):
+    print("INTRODUZCA DOS VALORES DISTINTOS")
+    A=int(input("A="))
+    B=int(input("B="))
+if A>B :
+    print(str(A)+ " es el mayor")
+else:
+    print(str(B)+ " es el mayor")
